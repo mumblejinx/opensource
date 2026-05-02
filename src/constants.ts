04fef04388b36b4ea79f71c55923d9671807d65a
mumblejinx <mumblejinx@gmail.com>
2026-05-02 23:31:44 +0900

    feat: Initialize Open Source Art project
    Sets up the basic project structure, including React, Tailwind CSS, and necessary configurations. Adds placeholder artwork data and README instructions for local development and deployment.

---
+++ src/constants.ts
@@ -0,0 +1,36 @@
+import { Artwork } from './types';
+
+export const ARTWORKS: Artwork[] = [
+  {
+    id: 'tv-week',
+    title: "Turn Off TV Week",
+    description: "Poster created for Adbusters' Turn Off TV Week.",
+    descriptionJp: "アボバスターズの『Turn Off TV Week』のために制作されたポスター。",
+    image: "https://picsum.photos/seed/tvweek/800/600",
+    downloadLink: "#"
+  },
+  {
+    id: 'manic-art',
+    title: "Manic-Art Promo",
+    description: "Promotional artwork for Manic-Art.",
+    descriptionJp: "Manic-Artのプロモーション用アートワーク。",
+    image: "https://picsum.photos/seed/manicart/800/600",
+    downloadLink: "#"
+  },
+  {
+    id: 'coloring-book',
+    title: "Coloring Book Pages",
+    description: "A set of experimental coloring pages.",
+    descriptionJp: "実験的なぬりえページのセット。",
+    image: "https://picsum.photos/seed/coloring/800/600",
+    downloadLink: "#"
+  },
+  {
+    id: 'outkast',
+    title: "Outkast Tribute",
+    description: "Artwork inspired by Outkast.",
+    descriptionJp: "Outkastにインスパイアされたアートワーク。",
+    image: "https://picsum.photos/seed/outkast/800/600",
+    downloadLink: "#"
+  }
+];
