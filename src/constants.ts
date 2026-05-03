31c82e5c20f20d705b8de903aa1978f651c75d97
Jake Galm <mumblejinx@gmail.com>
2026-05-03 09:26:20 +0900

    Add router + clean install

--- src/constants.ts
+++ src/constants.ts
@@ -1,52 +1,15 @@
-import { Artwork } from './types';
+const BASE = import.meta.env.BASE_URL;
 
-export const ARTWORKS: Artwork[] = [
+export const ARTWORKS = [
   {
     id: 'tv-week',
     title: "Turn Off TV Week",
-    description: "Poster created for Adbusters' Turn Off TV Week.",
-    descriptionJp: "アボバスターズの『Turn Off TV Week』のために制作されたポスター。",
-<<<<<<< HEAD
-    image: "https://picsum.photos/seed/tvweek/800/600",
-=======
-    image: "/images/tv-week/800/600",
->>>>>>> e66f69199c28f487561f324d4ddf027a92e0745e
-    downloadLink: "#"
-  },
-  {
-    id: 'manic-art',
-    title: "Manic-Art Promo",
-    description: "Promotional artwork for Manic-Art.",
-    descriptionJp: "Manic-Artのプロモーション用アートワーク。",
-<<<<<<< HEAD
-    image: "https://picsum.photos/seed/manicart/800/600",
-=======
-    image: "/images/manic-art/800/600",
->>>>>>> e66f69199c28f487561f324d4ddf027a92e0745e
-    downloadLink: "#"
-  },
-  {
-    id: 'coloring-book',
-    title: "Coloring Book Pages",
-    description: "A set of experimental coloring pages.",
-    descriptionJp: "実験的なぬりえページのセット。",
-<<<<<<< HEAD
-    image: "https://picsum.photos/seed/coloring/800/600",
-=======
-    image: "/images/coloring/800/600",
->>>>>>> e66f69199c28f487561f324d4ddf027a92e0745e
-    downloadLink: "#"
-  },
-  {
-    id: 'outkast',
-    title: "Outkast Tribute",
-    description: "Artwork inspired by Outkast.",
-    descriptionJp: "Outkastにインスパイアされたアートワーク。",
-<<<<<<< HEAD
-    image: "https://picsum.photos/seed/outkast/800/600",
-=======
-    image: "/images/outkast/800/600",
->>>>>>> e66f69199c28f487561f324d4ddf027a92e0745e
-    downloadLink: "#"
+
+    description: "...",
+    descriptionJp: "...",
+
+    image: `${BASE}artworks/tv-week/preview.jpg`,
+
+    page: `${BASE}artworks/tv-week/tv-week.html`
   }
 ];
